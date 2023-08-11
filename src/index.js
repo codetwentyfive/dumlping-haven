efc537ac58569dd855fe7073ad23b057941131f3
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-08-11 22:22:52 +0200

    feat:Add tab loading and switching function to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,57 @@
 import './styles.css';
-import { performInitialLoad } from './homepage';
+import { homepage } from './homepage';
+import { menu } from './menu';
+import { contact } from './contact';
+
+document.addEventListener('DOMContentLoaded', () => {
 
 
-performInitialLoad();
+
+
+    // Get references to the tabs
+    const menuTab = document.getElementById('menu-tab');
+    const contactTab = document.getElementById('contact-tab');
+    const homeTab = document.getElementById('home-tab');
+
+    // Get reference to the content container
+    const contentContainer = document.getElementById('content');
+
+    // Set up event listeners for tabs
+    menuTab.addEventListener('click', handleTabClick);
+    contactTab.addEventListener('click', handleTabClick);
+    homeTab.addEventListener('click', handleTabClick);
+
+    // Initially load the homepage content
+    homepage();
+    homeTab.classList.add('active'); // Mark the Home tab as active
+
+    // Function to handle tab clicks
+    function handleTabClick(event) {
+        const clickedTab = event.target;
+        const tabId = clickedTab.id;
+
+        // Clear content container
+        contentContainer.innerHTML = '';
+
+        // Update tab styling
+        clearActiveTabs();
+        clickedTab.classList.add('active');
+
+        // Load content based on the clicked tab
+        if (tabId === 'home-tab') {
+            homepage();
+        } else if (tabId === 'menu-tab') {
+            menu();
+        } else if (tabId === 'contact-tab') {
+            contact();
+        }
+    }
+
+    // Function to clear active styling from all tabs
+    function clearActiveTabs() {
+        menuTab.classList.remove('active');
+        contactTab.classList.remove('active');
+        homeTab.classList.remove('active');
+    }
+
+});
