98a51ac1f4db8548f985ae05a7e25f8923513499
codetwentyfive <chingisenkhbaatar@gmail.com>
2023-08-14 16:55:07 +0200

    fix:cleaned up some non needed code

--- src/index.js
+++ src/index.js
@@ -3,55 +3,51 @@ import { homepage } from './homepage';
 import { menu } from './menu';
 import { contact } from './contact';
 
-document.addEventListener('DOMContentLoaded', () => {
 
+// Get references to the tabs
+const menuTab = document.getElementById('menu-tab');
+const contactTab = document.getElementById('contact-tab');
+const homeTab = document.getElementById('home-tab');
 
+// Get reference to the content container
+const contentContainer = document.getElementById('content');
 
+// Set up event listeners for tabs
+menuTab.addEventListener('click', handleTabClick);
+contactTab.addEventListener('click', handleTabClick);
+homeTab.addEventListener('click', handleTabClick);
 
-    // Get references to the tabs
-    const menuTab = document.getElementById('menu-tab');
-    const contactTab = document.getElementById('contact-tab');
-    const homeTab = document.getElementById('home-tab');
+// Initially load the homepage content
+homepage();
+homeTab.classList.add('active'); // Mark the Home tab as active
 
-    // Get reference to the content container
-    const contentContainer = document.getElementById('content');
+// Function to handle tab clicks
+function handleTabClick(event) {
+    const clickedTab = event.target;
+    const tabId = clickedTab.id;
 
-    // Set up event listeners for tabs
-    menuTab.addEventListener('click', handleTabClick);
-    contactTab.addEventListener('click', handleTabClick);
-    homeTab.addEventListener('click', handleTabClick);
+    // Clear content container
+    contentContainer.innerHTML = '';
 
-    // Initially load the homepage content
-    homepage();
-    homeTab.classList.add('active'); // Mark the Home tab as active
+    // Update tab styling
+    clearActiveTabs();
+    clickedTab.classList.add('active');
 
-    // Function to handle tab clicks
-    function handleTabClick(event) {
-        const clickedTab = event.target;
-        const tabId = clickedTab.id;
-
-        // Clear content container
-        contentContainer.innerHTML = '';
-
-        // Update tab styling
-        clearActiveTabs();
-        clickedTab.classList.add('active');
-
-        // Load content based on the clicked tab
-        if (tabId === 'home-tab') {
-            homepage();
-        } else if (tabId === 'menu-tab') {
-            menu();
-        } else if (tabId === 'contact-tab') {
-            contact();
-        }
+    // Load content based on the clicked tab
+    if (tabId === 'home-tab') {
+        homepage();
+    } else if (tabId === 'menu-tab') {
+        menu();
+    } else if (tabId === 'contact-tab') {
+        contact();
     }
+}
 
-    // Function to clear active styling from all tabs
-    function clearActiveTabs() {
-        menuTab.classList.remove('active');
-        contactTab.classList.remove('active');
-        homeTab.classList.remove('active');
-    }
+// Function to clear active styling from all tabs
+function clearActiveTabs() {
+    menuTab.classList.remove('active');
+    contactTab.classList.remove('active');
+    homeTab.classList.remove('active');
+}
 
-});
+homepage();
